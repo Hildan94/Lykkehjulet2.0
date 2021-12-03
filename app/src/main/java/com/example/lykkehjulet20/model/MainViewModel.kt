d414d90bd17497e52fbf2725833ea2c14c76a487
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-03 22:14:03 +0100

    changed the type for score and lives

--- app/src/main/java/com/example/lykkehjulet20/model/MainViewModel.kt
+++ app/src/main/java/com/example/lykkehjulet20/model/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.lykkehjulet20.data.highscores
 import com.example.lykkehjulet20.data.listOfWords
 import java.text.NumberFormat
 
@@ -13,9 +14,19 @@ class MainViewModel : ViewModel() {
 
     // Score for the player
     private val _score = MutableLiveData(0)
-    val score: LiveData<String> = Transformations.map(_score) {
-        NumberFormat.getCurrencyInstance().format(it)
-    }
+    val score: LiveData<Int?> = _score
+
+    // Total cost of the order
+    private val _lives = MutableLiveData(5)
+    val lives: LiveData<Int?> = _lives
+
+
+
+    // Initialize data.
+    val myDataset = highscores().loadHighscores()
+
+    private var userName: String = "s164539"
+
     private lateinit var currentWord: String
 
     init {
@@ -26,9 +37,18 @@ class MainViewModel : ViewModel() {
     currentWord = listOfWords.random()
     }
 
+    fun isWordGuessed(playerWord: String): Boolean {
+        if(playerWord.equals(currentWord, true)){
+
+            return true
+            }
+        return false
+    }
+
     fun restartGame() {
         _score.value = 0
 
     }
+
 
 }
