a94252b86e52144d744eba282835e639ce9c2183
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-03 20:52:00 +0100

    added binding to the class

--- app/src/main/java/com/example/lykkehjulet20/ui/main/WinFragment.kt
+++ app/src/main/java/com/example/lykkehjulet20/ui/main/WinFragment.kt
@@ -6,15 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.lykkehjulet20.R
+import com.example.lykkehjulet20.databinding.StartFragmentBinding
+import com.example.lykkehjulet20.databinding.WinFragmentBinding
+import com.example.lykkehjulet20.model.MainViewModel
+import kotlin.system.exitProcess
+import androidx.core.app.ActivityCompat.finishAffinity as finishAffinity1
 
 class WinFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = WinFragment()
-    }
 
-    private lateinit var viewModel: MainViewModel
+    private var _binding: WinFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val sharedViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +30,13 @@ class WinFragment : Fragment() {
         return inflater.inflate(R.layout.win_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    fun restartGame(){
+    sharedViewModel.restartGame()
+
+    findNavController().navigate(R.id.action_winFragment_to_startFragment)
     }
+
+
+
 
 }
