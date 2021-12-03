2593a9ce5fef42f56b48051f56fd2d928ae0f6d8
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-03 20:49:52 +0100

    added binding

--- app/src/main/java/com/example/lykkehjulet20/MainActivity.kt
+++ app/src/main/java/com/example/lykkehjulet20/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.lykkehjulet20
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.lykkehjulet20.databinding.MainActivityBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
