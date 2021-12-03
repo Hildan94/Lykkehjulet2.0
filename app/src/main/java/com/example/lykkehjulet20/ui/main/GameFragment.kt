5a791002bb377a35c785d7163924d213adb32d4f
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-04 00:04:09 +0100

    added a guess function set the button listeners and the function to it

--- app/src/main/java/com/example/lykkehjulet20/ui/main/GameFragment.kt
+++ app/src/main/java/com/example/lykkehjulet20/ui/main/GameFragment.kt
@@ -36,6 +36,35 @@ class GameFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
             gameFragment = this@GameFragment
+
+            guessButton.setOnClickListener{guess()}
+            replayButton.setOnClickListener{replayGame()}
+        }
+    }
+
+
+    private fun guess() {
+        val playerWord = binding.textInputEditText.text.toString()[0]
+        sharedViewModel.guessLetter(playerWord)
+        if(sharedViewModel.isWordGuessed()) {
+            win()
+        }
+        else
+            sharedViewModel.decreaseLife()
+        if(sharedViewModel.lives.value == 0) {
+        lose()
+        }
+    }
+
+    fun win() {
+        binding.guessButton.setOnClickListener {
+            findNavController().navigate(R.id.action_GameFragment_to_winFragment)
+        }
+    }
+
+    fun lose() {
+        binding.guessButton.setOnClickListener {
+            findNavController().navigate(R.id.action_GameFragment_to_loseFragment)
         }
     }
 
@@ -44,9 +73,6 @@ class GameFragment : Fragment() {
         findNavController().navigate(R.id.action_GameFragment_to_startFragment)
     }
 
-    fun guessLetter() {
-
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
