da919bbe62add9cc625a1b06c72b677ac3093cde
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-03 20:51:08 +0100

    added binding to the class

--- app/src/main/java/com/example/lykkehjulet20/ui/main/StartFragment.kt
+++ app/src/main/java/com/example/lykkehjulet20/ui/main/StartFragment.kt
@@ -1,32 +1,43 @@
 package com.example.lykkehjulet20.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lykkehjulet20.R
+import com.example.lykkehjulet20.model.MainViewModel
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.lykkehjulet20.databinding.StartFragmentBinding
 
 class StartFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StartFragment()
-    }
+    private val viewModel: MainViewModel by viewModels()
 
-    private lateinit var viewModel: MainViewModel
+    private var _binding: StartFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.start_fragment, container, false)
+        _binding = StartFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        // Navigate to entree menu
+        binding.startbutton.setOnClickListener {
+            // TODO: navigate to the EntreeMenuFragment
+            findNavController().navigate(R.id.action_startFragment_to_GameFragment)
+        }
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
