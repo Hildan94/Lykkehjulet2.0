bcaa1231f8670b10f3284ba35574890577b8460e
Hildibjørg <s164539@stundet.dtu.dk>
2021-12-03 14:41:38 +0100

    Added the navController to MainActivity.kt

--- app/src/main/java/com/example/lykkehjulet20/MainActivity.kt
+++ app/src/main/java/com/example/lykkehjulet20/MainActivity.kt
@@ -2,17 +2,24 @@ package com.example.lykkehjulet20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lykkehjulet20.ui.main.StartFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, StartFragment.newInstance())
-                .commitNow()
-        }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp() }
 }
